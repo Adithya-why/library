4acaad0482907958f18b463835d1ffbfedf5e89b
Adi <adithiyans123@gmail.com>
2023-03-06 11:26:43 +0530

    enabled the feature to add books

--- main.js
+++ main.js
@@ -13,16 +13,7 @@ Book.prototype.info = function(){
     return this.name + this.author + this.noOfPages+ this.haveRead;
 }
 
-function addToBooks(){
-    let n,a,nop,rd;
-    n = prompt("ENter the name of the book");
-    a = prompt("ENter the author of the book");
-    nop = parseInt(prompt("ENter the no. of pages"));
-    rd = prompt("Have you read the book(true/false)");
 
-    let book = new Book(n,a,nop,rd);
-    myLibrary.push(book);
-}
 
 const b1 = new Book("Tom sawyer","Mark twain",100,true);
 const b2 = new Book("Tom sawyer","Mark twain",100,true);
@@ -35,7 +26,6 @@ myLibrary.push(b3);
 myLibrary.push(b4);
 
 
-
 for(let i=0;i<myLibrary.length;i++){
     const div = document.createElement("div");
     div.classList.add(`bkno`);
@@ -43,6 +33,7 @@ for(let i=0;i<myLibrary.length;i++){
     const inDivName = document.createElement("div");
     inDivName.innerHTML = myLibrary[i].name;
 
+    
     const inDivAuth = document.createElement("div");
     inDivAuth.innerHTML = myLibrary[i].author;
 
@@ -62,8 +53,42 @@ for(let i=0;i<myLibrary.length;i++){
 }
 
 
-
 const btn = document.querySelector('button');
 btn.addEventListener('click',(e)=>{
-    
+    const bname = document.querySelector("#bname").value;
+    const bauth = document.querySelector("#bauth").value;
+    const bnop = document.querySelector("#bnop").value;
+    const brd = document.querySelector("#brd").checked;
+
+    const b = new Book(bname,bauth,parseInt(bnop),brd)
+    myLibrary.push(b);
+    const div = document.createElement("div");
+    div.classList.add(`bkno`);
+    div.classList.add(`${myLibrary.length-1}`);
+    const inDivName = document.createElement("div");
+    inDivName.innerHTML = myLibrary[myLibrary.length-1].name;
+
+    const inDivAuth = document.createElement("div");
+    inDivAuth.innerHTML = myLibrary[myLibrary.length-1].author;
+
+    const inDivNop = document.createElement("div");
+    inDivNop.innerHTML = myLibrary[myLibrary.length-1].noOfPages;
+
+    const inDivRd = document.createElement("div");
+    inDivRd.innerHTML = myLibrary[myLibrary.length-1].haveRead;
+
+    div.appendChild(inDivName);
+    div.appendChild(inDivAuth);
+    div.appendChild(inDivNop);
+    div.appendChild(inDivRd);
+
+
+    bks.appendChild(div);
+
 });
+
+
+
+
+
+
